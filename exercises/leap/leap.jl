caf3f6bab8500f9a396657e4b39908b18392511d
Matheus Pedroni <pnmatheus@protonmail.com>
2023-04-02 17:27:40 -0300

    leap exercise

---
+++ exercises/leap/leap.jl
@@ -0,0 +1,15 @@
+"""
+    is_leap_year(year)
+
+Return `true` if `year` is a leap year in the gregorian calendar.
+
+"""
+function is_leap_year(year)
+    if year % 4 == 0
+        if year % 100 != 0 || year % 400 == 0
+            return true
+        end
+    end
+    return false
+end
+
